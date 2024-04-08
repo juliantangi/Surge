d8d085d117b3c9e9a862d82492bc5867607622f6
juliantangi <56528263+juliantangi@users.noreply.github.com>
2024-04-09 03:06:23 +0800

    Update Ponte Switcher.js

--- Ponte Switcher.js
+++ Ponte Switcher.js
@@ -1,79 +1,40 @@
-class httpMethod {
-    /**
-     * Callback function
-     * @param {*} resolve 
-     * @param {*} reject 
-     * @param {*} error 
-     * @param {*} response 
-     * @param {*} data 
-     */
-    static _httpRequestCallback(resolve, reject, error, response, data) {
-        if (error) {
-            reject(error);
-        } else {
-            resolve(Object.assign(response, { data }));
-        }
-    }
+const REQUEST_HEADERS = {
+	'Accept':'application/json'
+};
 
-    /**
-     * HTTP GET
-     * @param {Object} option 選項
-     * @returns 
-     */
-    static get(option = {}) {
-        return new Promise((resolve, reject) => {
-            $httpClient.get(option, (error, response, data) => {
-                this._httpRequestCallback(resolve, reject, error, response, data);
-            });
-        });
-    }
-
-    /**
-     * HTTP POST
-     * @param {Object} option 选项
-     * @returns 
-     */
-    static post(option = {}) {
-        return new Promise((resolve, reject) => {
-            $httpClient.post(option, (error, response, data) => {
-                this._httpRequestCallback(resolve, reject, error, response, data);
-            });
-        });
-    }
-}
-
-class loggerUtil {
-    constructor() {
-        this.id = randomString();
-    }
-
-    log(message) {
-        message = `[${this.id}] [ LOG ] ${message}`;
-        console.log(message);
-    }
-
-    error(message) {
-        message = `[${this.id}] [ERROR] ${message}`;
-        console.log(message);
-    }
-}
-
-function getNetworkInfo(retryTimes = 5, retryInterval = 1000) {
-    // send http request
-    httpMethod.get('http://ip-api.com/json').then(response => {
-        if (Number(response.status) > 300) {
-            throw new Error(`Request error with http status code: ${response.status}\n${response.data}`);
-        }
-        const info = JSON.parse(response.data);
-        let networkinfo=info.query+info.country
-        return networkinfo
-    }).catch(error => {
-        return 'unknown'
+function getNetworkInfo() {
+  return new Promise((resolve, reject) => {
+    let option = {
+      url: 'https://api.ipdata.co/?api-key=e63e9feb223c15f832b756e4f4a6a68894bc3ba13fa86066df28254d',
+      headers:REQUEST_HEADERS
+    };
+    $httpClient.get(option, function (error, response, data) {
+      if (error) {
+        reject('error unknown');
+      } else if (response.status === 200) {
+        const info = JSON.parse(data);
+        let networkinfo = info.query + info.country;
+        resolve(networkinfo);
+      } else {
+				errorcode=response.status
+        resolve(errorcode);
+      }
     });
+  });
 }
 
-const retryTimes = 5;
-const retryInterval = 1000;
-let NetworkInfo=getNetworkInfo(retryTimes, retryInterval);
-console.log(NetworkInfo);
-$done();
+async function main() {
+  try {
+    const NetworkInfo = await getNetworkInfo();
+    if (NetworkInfo!= 'Hong Kong'){
+			$surge.setSelectGroupPolicy('Ponte', 'Hong Kong');
+    $notification.post('SFW Check', '','IP address has been switched to Hong Kong.');
+		}
+		
+  } catch (error) {
+    console.log(error);
+  }
+  $done();
+}
+
+main();
